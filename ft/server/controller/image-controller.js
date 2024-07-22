d80159c819a9c2152af131156306d0153b3a8ff6
Adnan Kundlik <147232252+akk1310@users.noreply.github.com>
2024-07-22 21:53:31 +0530

    Update image-controller.js

--- ft/server/controller/image-controller.js
+++ ft/server/controller/image-controller.js
@@ -9,7 +9,7 @@ export const uploadImage = async (request,response)=>{
     }
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({path: `http://localhost:8000/file/${file._id}`})
+        response.status(200).json({path: `https://filetransfer-backend-tklk.onrender.com/file/${file._id}`})
         
     } catch (error) {
         console.error(error.message)
