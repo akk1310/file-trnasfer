0720ed1c8813bada03cf4202fc79b23dfabbb0de
Adnan Kundlik <147232252+akk1310@users.noreply.github.com>
2024-07-28 13:21:28 +0530

    Add files via upload

---
+++ ft/client/src/Images/index.js
@@ -0,0 +1,9 @@
+const IMAGES = {
+    imgOne : require('./nature1.jpg'),
+    imgTwo : require('./nature2.jpg'),
+    imgThree : require('./nature3.jpg'),
+    img4 : require('./nature4.jpeg'),
+    img5 : require('./nature5.jpg'),
+}
+
+export default IMAGES
